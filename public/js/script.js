3bdf16be450116add2edf6f6c927061c30ccd041
Benjamin Bulone <benja.bulone@gmail.com>
2022-03-15 13:11:14 +0100

    lancement

---
+++ public/js/script.js
@@ -0,0 +1,41 @@
+var menuMobile = document.getElementById("testmobile");
+
+import { initializeApp } from "firebase-admin/app";
+
+var serviceAccount = require("portfolio-d55bc-firebase-adminsdk-h98w6-86d99c8552.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+function openNav() {
+  $(".nav2").slideDown("slow", function () {
+    $(".fa-bars").addClass("display-none");
+    $(".nav2").css("display", "flex");
+    $(".fa-times").removeClass("display-none");
+    $(".fa-times").addClass("display-croix");
+  });
+}
+
+function closeNav() {
+  $(".nav2").slideUp("slow", function () {
+    $(".nav2").css("display", "none");
+    $(".fa-times").removeClass("display-croix");
+    $(".fa-times").addClass("display-none");
+    $(".fa-bars").removeClass("display-none");
+    $(".fa-bars").addClass("display-block");
+  });
+}
+
+$(document).ready(function () {
+  $("#owl-demo").owlCarousel({
+    navigation: true, // Show next and prev buttons
+    autoplay: true,
+    slideSpeed: 30,
+    paginationSpeed: 90,
+
+    loop: true,
+    autoplayTimeout: 2500,
+    items: 1,
+  });
+});
